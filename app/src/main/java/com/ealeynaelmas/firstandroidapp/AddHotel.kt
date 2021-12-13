78a7684d284fcab10cfe87289a813c8f738d9c80
E-AleynaElmas <a.aleynaelmas@gmail.com>
2021-12-14 00:20:04 +0300

    Firebase connected. And added "Add Hotel to database feature".

--- app/src/main/java/com/ealeynaelmas/firstandroidapp/AddHotel.kt
+++ app/src/main/java/com/ealeynaelmas/firstandroidapp/AddHotel.kt
@@ -1,11 +1,19 @@
 package com.ealeynaelmas.firstandroidapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.view.View
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_add_hotel.*
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 class AddHotel : AppCompatActivity() {
+    var database = FirebaseDatabase.getInstance().reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_hotel)
@@ -15,5 +23,37 @@ class AddHotel : AppCompatActivity() {
     fun loadMainPage(view : View){
         val intent = Intent(applicationContext, MainActivity ::class.java)
         startActivity(intent)
+    }
+
+    fun reload(){
+        val intent = Intent(applicationContext, AddHotel ::class.java)
+        startActivity(intent)
+    }
+
+    fun addHotelToDB(view: View){
+        var hotelCity = HotelCity.text.toString()
+        var hotelName = HotelName.text.toString()
+        var hotelLicanceNo = LicanceNo.text.toString().toLong()
+        var hoteDocumentNo = DocumentNo.text.toString().toInt()
+        var hotelCost = HotelCost.text.toString().toInt()
+        var hotelLocation = HotelLocation.text.toString()
+
+        var attr1 = Attribute1.isChecked()
+        var attr2 = Attribute2.isChecked()
+        var attr3 = Attribute3.isChecked()
+        var attr4 = Attribute4.isChecked()
+        var attr5 = Attribute5.isChecked()
+        var attr6 = Attribute6.isChecked()
+
+        /*database.setValue(Hotel(hotelCity, hotelName,hotelLicanceNo,
+            hotelCost,hotelLocation,hoteDocumentNo,attr1, attr2, attr3,
+            attr4, attr5, attr6))*/
+       var myId = database.push().key
+
+        database.child("Hotels").child(myId.toString()).setValue(Hotel(hotelCity, hotelName,hotelLicanceNo,
+            hotelCost,hotelLocation,hoteDocumentNo,attr1, attr2, attr3,
+            attr4, attr5, attr6))
+
+        reload()
     }
 }
---
+++ app/src/main/java/com/ealeynaelmas/firstandroidapp/Rezervation.kt
@@ -0,0 +1,22 @@
+package com.ealeynaelmas.firstandroidapp
+
+import java.util.*
+
+class Rezervation {
+    var rhotelname:String = ""
+    var rpersoncount:Int = 0
+    var rchilcount:Int = 0
+    var rchecindate:String = ""
+    var rcheckoutdate:String = ""
+    var rcontactname:String = ""
+    var rcontactsurname:String = ""
+    var rcontactemail:String = ""
+    var rcontactphonenumber:String = ""
+
+    var rpersonname: String = ""
+    var rpersonsurname: String = ""
+    var rpersontc: Long = 0
+    var rpersonphonenumber: String = ""
+    var rspecialrequest: Boolean = false
+    var rspecialrequestText = ""
+}
